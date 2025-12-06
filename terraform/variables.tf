25a23fc809935f7b0469aedec36a705f339bd79c
Antônio Elvis <102705791+Antonio-Elvis-Dev@users.noreply.github.com>
2025-12-05 21:16:36 -0300

    feat - implementação do terraform

---
+++ terraform/variables.tf
@@ -0,0 +1,9 @@
+variable "do_token" {
+  description = "DigitalOcean API Token"
+  type        = string
+}
+
+variable "ssh_fingerprint" {
+  description = "Fingerprint da chave SSH adicionada na DigitalOcean"
+  type        = string
+}
